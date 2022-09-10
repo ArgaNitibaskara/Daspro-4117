69f7fc7ad05a11021035c5850565e383c13f6e13
ArgaNitibaskara <111202214642@mhs.dinus.ac.id>
2022-09-10 08:14:03 +0700

    Create main.cpp

---
+++ Tugas1/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello world!" << std::endl;
+    std::cout << "Nama: Tsaabitah Arga Nitibaskara" << std::endl;
+    std::cout << "NIM: A11.2022.14642" << std::endl;
+    std::cout << "Asal Sekolah: SMAN 2 Klaten" << std::endl;
+    return 0;
+}
