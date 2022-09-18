4e80c2257621c9a687c97d8f790f0b9e79443524
ArgaNitibaskara <111202214642@mhs.dinus.ac.id>
2022-09-18 09:22:41 +0700

    Create Pertemuan3.cpp

---
+++ Pertemuan3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+int main()
+{
+    printf("Paladin");
+    int x = 3000;
+    std::cout << "\nGold:" << x;
+    int l = 100;
+    float kd = 3.5;
+    char s = 'S';
+    char d = 'S';
+    char a = 'B';
+    std::string r = "SSR";
+
+    std::cout << "\nlevel:" << l;
+    std::cout << "\nK/D:" << kd;
+    std::cout << "\nStr:" << s;
+    std::cout << "\nDef:" << d;
+    std::cout << "\nAgi:" << a;
+    std::cout << "\nRarity:" << r;
+
+    return 0;
+}
